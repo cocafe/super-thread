4142cbd7addef4dfac387b42cc489067f15b7410
cocafe <cocafehj@gmail.com>
2022-02-21 21:53:34 +0800

    ntapi: comment out conflicted macros
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- src/myntapi.h
+++ src/myntapi.h
@@ -80,26 +80,26 @@ typedef struct _PAGE_PRIORITY_INFORMATION
 } PAGE_PRIORITY_INFORMATION, *PPAGE_PRIORITY_INFORMATION;
 
 #define MEMORY_PRIORITY_LOWEST          0
-#define MEMORY_PRIORITY_VERY_LOW        1
-#define MEMORY_PRIORITY_LOW             2
-#define MEMORY_PRIORITY_MEDIUM          3
-#define MEMORY_PRIORITY_BELOW_NORMAL    4
-#define MEMORY_PRIORITY_NORMAL          5
-
-#define THREAD_BASE_PRIORITY_LOWRT      15      // value that gets a thread to LowRealtime-1
-#define THREAD_BASE_PRIORITY_MAX        2       // maximum thread base priority boost
-#define THREAD_BASE_PRIORITY_MIN        (-2)    // minimum thread base priority boost
-#define THREAD_BASE_PRIORITY_IDLE       (-15)   // value that gets a thread to idle
-
-#define THREAD_PRIORITY_LOWEST          THREAD_BASE_PRIORITY_MIN
-#define THREAD_PRIORITY_BELOW_NORMAL    (THREAD_PRIORITY_LOWEST+1)
-#define THREAD_PRIORITY_NORMAL          0
-#define THREAD_PRIORITY_HIGHEST         THREAD_BASE_PRIORITY_MAX
-#define THREAD_PRIORITY_ABOVE_NORMAL    (THREAD_PRIORITY_HIGHEST-1)
-#define THREAD_PRIORITY_ERROR_RETURN    (MAXLONG)
-
-#define THREAD_PRIORITY_TIME_CRITICAL   THREAD_BASE_PRIORITY_LOWRT
-#define THREAD_PRIORITY_IDLE            THREAD_BASE_PRIORITY_IDLE
+//#define MEMORY_PRIORITY_VERY_LOW        1
+//#define MEMORY_PRIORITY_LOW             2
+//#define MEMORY_PRIORITY_MEDIUM          3
+//#define MEMORY_PRIORITY_BELOW_NORMAL    4
+//#define MEMORY_PRIORITY_NORMAL          5
+//
+//#define THREAD_BASE_PRIORITY_LOWRT      15      // value that gets a thread to LowRealtime-1
+//#define THREAD_BASE_PRIORITY_MAX        2       // maximum thread base priority boost
+//#define THREAD_BASE_PRIORITY_MIN        (-2)    // minimum thread base priority boost
+//#define THREAD_BASE_PRIORITY_IDLE       (-15)   // value that gets a thread to idle
+//
+//#define THREAD_PRIORITY_LOWEST          THREAD_BASE_PRIORITY_MIN
+//#define THREAD_PRIORITY_BELOW_NORMAL    (THREAD_PRIORITY_LOWEST+1)
+//#define THREAD_PRIORITY_NORMAL          0
+//#define THREAD_PRIORITY_HIGHEST         THREAD_BASE_PRIORITY_MAX
+//#define THREAD_PRIORITY_ABOVE_NORMAL    (THREAD_PRIORITY_HIGHEST-1)
+//#define THREAD_PRIORITY_ERROR_RETURN    (MAXLONG)
+//
+//#define THREAD_PRIORITY_TIME_CRITICAL   THREAD_BASE_PRIORITY_LOWRT
+//#define THREAD_PRIORITY_IDLE            THREAD_BASE_PRIORITY_IDLE
 
 typedef enum _PROC_PRIORITY_HINT {
         ProcPrioClassUnknown             = 0,
